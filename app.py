d0e347e510e10743df38df9a0735dbcddef3ad61
Aditya Mahes <91634844+adityamahes@users.noreply.github.com>
2024-05-20 12:28:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
-import streamlit
+import streamlit as st
 from predict_page import show_predict_page
-show_predict_page()
+
+try:
+    show_predict_page()
+except:
+    st.write("""###### Invalid Input ######""")
