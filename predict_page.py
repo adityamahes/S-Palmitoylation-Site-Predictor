6c4b5b4390e7209875d1a32b47b71c919652b33e
Aditya Mahes <91634844+adityamahes@users.noreply.github.com>
2024-05-20 12:27:57 -0400

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
-from tensorflow import keras
-from data_processing import encode, sequenced, truncate
+import keras
+import pandas as pd
+from data_processing import encode, sequenced, truncate, c_position_list
 
 def load_model():
     data = keras.layers.TFSMLayer(filepath='S_Palmitoylation_Modelexport', call_endpoint='serving_default')
@@ -17,30 +18,49 @@ def show_predict_page():
     if type_input == "Accession":
         st.write("""#### Enter the following ####""")
         accession = st.text_input("Accession", "P21279")
-        position = int(st.text_input("Position of Cystiene Residue", "10"))
-        truncated_sequence = truncate(sequenced(accession), position, 100)
+        # position = int(st.text_input("Position of Cystiene Residue", "10"))
+        amino_acid_sequence = sequenced(accession)
+        position_list = c_position_list(amino_acid_sequence)
+        truncated_sequence_list = []
+        encoded_sequence_list = []
+        for pos in position_list:
+            truncated = truncate(amino_acid_sequence, pos, 100)
+            truncated_sequence_list.append(truncated[80:120])
+            encoded_sequence_list.append(encode(truncated))
 
-        try:
-            input = np.array([encode(truncated_sequence)])
-        except:
-            st.write("""###### Invalid Sequence ######""")
+
     else:
         st.write("""#### Enter a raw sequence ####""")
         raw_sequence = st.text_area(
             "Raw Sequence", 
             "MTLESIMACCLSEEAKEARRINDEIERHVRRDKRDARRELKLLLLGTGESGKSTFIKQMRIIHGSGYSDEDKRGFTKLVYQNIFTAMQAMIRAMDTLKIPYKYEHNKAHAQLVREVDVEKVSAFENPYVDAIKSLWNDPGIQECYDRRREYQLSDSTKYYLNDLDRVADPSYLPTQQDVLRVRVPTTGIIEYPFDLQSVIFRMVDVGGQRSERRKWIHCFENVTSIMFLVALSEYDQVLVESDNENRMEESKALFRTIITYPWFQNSSVILFLNKKDLLEEKIMYSHLVDYFPEYDGPQRDAQAAREFILKMFVDLNPDSDKIIYSHFTCATDTENIRFVFAAVKDTILQLNLKEYNLV").strip(" ")
-        position = int(st.text_input("Position of Cystiene Residue", "10"))
-        truncated_sequence = truncate(raw_sequence, position, 100)
-
-        try:
-            input = np.array([encode(truncated_sequence)])
-        except:
-            st.write("""###### Invalid Sequence ######""")
+        # position = int(st.text_input("Position of Cystiene Residue", "10"))
+        position_list = c_position_list(raw_sequence)
+        truncated_sequence_list = []
+        encoded_sequence_list = []
+        for pos in position_list:
+            truncated = truncate(raw_sequence, pos, 100)
+            truncated_sequence_list.append(truncated[80:120])
+            encoded_sequence_list.append(encode(truncated))
     
-    ok = st.button("Calculate Likeliness of Being an S Palmitoylation Site")
+    input = np.array(encoded_sequence_list)
+    
+    ok = st.button("S Palmitoylation Site Predictions for all Cystiene Residues")
+
 
     data = load_model()
-    print(data(input))
-    output = data(input)["output_0"].numpy()[0][0] * 100
+
+
+    output = data(input)["output_0"].numpy()
+    
+
+    table = {
+        "Position": position_list,
+        "Likeliness of being an S Palmitoylation Site": [str(round(i[0] * 100, 2)) + "%" for i in output.tolist()],
+        "Truncated Sequence": truncated_sequence_list
+    }
+    df = pd.DataFrame(table)
+
     if ok:
-        st.subheader(f"The likeliness of it being an S Palmitoylation Site is {output:.2f}%")
+        st.subheader(f"S Palmitoylation Site Prediction for all Cystiene Residues")
+        st.table(df)
